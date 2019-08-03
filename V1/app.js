0e3dbbe7e0c80b6f8eed5e1ff809151787313073
John Kim <johnseunkyukim@gmail.com>
2019-08-02 22:35:00 -0400

    Added server functionality

---
+++ V1/app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("styles")); //css files storage
+app.use(express.static("scripts")); //scripts files storage (important for MVC)
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("Server is now active!");
+});
