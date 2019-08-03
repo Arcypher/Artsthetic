6f1ab27244226d85d50fb0e0f2c9069caf170f95
John Kim <johnseunkyukim@gmail.com>
2019-08-03 17:23:48 -0400

    Comments

--- V1/app.js
+++ V1/app.js
@@ -5,10 +5,12 @@ app.use(express.static("styles")); //css files storage
 app.use(express.static("scripts")); //scripts files storage (important for MVC)
 app.set("view engine", "ejs");
 
+//root page
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
+//automatically collect IP. Set port through command prompt
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Server is now active!");
 });
